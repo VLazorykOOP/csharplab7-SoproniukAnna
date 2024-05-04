580a69ffeaae1f5c8ac555b7c4d96d15d9e46f15
SoproniukAnna <124136300+SoproniukAnna@users.noreply.github.com>
2024-05-04 18:05:36 +0300

    Final

--- Lab7CSharp/Form1.Designer.cs
+++ Lab7CSharp/Form1.Designer.cs
@@ -1,17 +1,21 @@
-﻿
-namespace Lab7CSharp
+﻿namespace WindowsFormsApp
 {
     partial class Form1
     {
-        /// <summary>
-        /// Обязательная переменная конструктора.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.TextBox textBoxInfo;
+        private System.Windows.Forms.Button buttonInfo;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.ComboBox comboBoxShape;
+        private System.Windows.Forms.Button buttonDraw;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.Button buttonLoadImage;
+        private System.Windows.Forms.Button buttonSaveImage;
+        private System.Windows.Forms.Button buttonMirrorImage;
 
-        /// <summary>
-        /// Освободить все используемые ресурсы.
-        /// </summary>
-        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -21,48 +25,150 @@ namespace Lab7CSharp
             base.Dispose(disposing);
         }
 
-        #region Код, автоматически созданный конструктором форм Windows
-
-        /// <summary>
-        /// Требуемый метод для поддержки конструктора — не изменяйте 
-        /// содержимое этого метода с помощью редактора кода.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxInfo = new System.Windows.Forms.TextBox();
+            this.buttonInfo = new System.Windows.Forms.Button();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.comboBoxShape = new System.Windows.Forms.ComboBox();
+            this.buttonDraw = new System.Windows.Forms.Button();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.buttonLoadImage = new System.Windows.Forms.Button();
+            this.buttonSaveImage = new System.Windows.Forms.Button();
+            this.buttonMirrorImage = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
             this.SuspendLayout();
             // 
-            // label1
+            // textBoxInfo
             // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.PaleGreen;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.World, ((byte)(204)));
-            this.label1.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.label1.Location = new System.Drawing.Point(376, -1);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(95, 18);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Lab 7.   C# ";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.textBoxInfo.Location = new System.Drawing.Point(60, 74);
+            this.textBoxInfo.Name = "textBoxInfo";
+            this.textBoxInfo.Size = new System.Drawing.Size(200, 20);
+            this.textBoxInfo.TabIndex = 0;
+            this.textBoxInfo.TextChanged += new System.EventHandler(this.textBoxInfo_TextChanged);
+            // 
+            // buttonInfo
+            // 
+            this.buttonInfo.Location = new System.Drawing.Point(60, 114);
+            this.buttonInfo.Name = "buttonInfo";
+            this.buttonInfo.Size = new System.Drawing.Size(100, 30);
+            this.buttonInfo.TabIndex = 8;
+            this.buttonInfo.Text = "Інформація";
+            this.buttonInfo.UseVisualStyleBackColor = true;
+            this.buttonInfo.Click += new System.EventHandler(this.buttonInfo_Click);
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Location = new System.Drawing.Point(166, 114);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(100, 30);
+            this.buttonExit.TabIndex = 7;
+            this.buttonExit.Text = "Вихід";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.MouseHover += new System.EventHandler(this.buttonExit_MouseHover);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(350, 50);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(754, 533);
+            this.pictureBox.TabIndex = 3;
+            this.pictureBox.TabStop = false;
+            // 
+            // comboBoxShape
+            // 
+            this.comboBoxShape.FormattingEnabled = true;
+            this.comboBoxShape.Items.AddRange(new object[] {
+            "Коло",
+            "Квадрат",
+            "Прямокутник"});
+            this.comboBoxShape.Location = new System.Drawing.Point(60, 199);
+            this.comboBoxShape.Name = "comboBoxShape";
+            this.comboBoxShape.Size = new System.Drawing.Size(200, 21);
+            this.comboBoxShape.TabIndex = 4;
+            // 
+            // buttonDraw
+            // 
+            this.buttonDraw.Location = new System.Drawing.Point(116, 254);
+            this.buttonDraw.Name = "buttonDraw";
+            this.buttonDraw.Size = new System.Drawing.Size(100, 30);
+            this.buttonDraw.TabIndex = 6;
+            this.buttonDraw.Text = "Намалювати";
+            this.buttonDraw.UseVisualStyleBackColor = true;
+            this.buttonDraw.Click += new System.EventHandler(this.buttonDraw_Click);
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(12, 315);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(315, 150);
+            this.dataGridView.TabIndex = 5;
+            this.dataGridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView_CellContentClick);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            // 
+            // saveFileDialog
+            // 
+            this.saveFileDialog.FileName = "saveFileDialog";
+            // 
+            // buttonLoadImage
+            // 
+            this.buttonLoadImage.Location = new System.Drawing.Point(32, 503);
+            this.buttonLoadImage.Name = "buttonLoadImage";
+            this.buttonLoadImage.Size = new System.Drawing.Size(100, 30);
+            this.buttonLoadImage.TabIndex = 2;
+            this.buttonLoadImage.Text = "Завантажити";
+            this.buttonLoadImage.UseVisualStyleBackColor = true;
+            this.buttonLoadImage.Click += new System.EventHandler(this.buttonLoadImage_Click);
+            // 
+            // buttonSaveImage
+            // 
+            this.buttonSaveImage.Location = new System.Drawing.Point(116, 553);
+            this.buttonSaveImage.Name = "buttonSaveImage";
+            this.buttonSaveImage.Size = new System.Drawing.Size(100, 30);
+            this.buttonSaveImage.TabIndex = 1;
+            this.buttonSaveImage.Text = "Зберегти";
+            this.buttonSaveImage.UseVisualStyleBackColor = true;
+            this.buttonSaveImage.Click += new System.EventHandler(this.buttonSaveImage_Click);
+            // 
+            // buttonMirrorImage
+            // 
+            this.buttonMirrorImage.Location = new System.Drawing.Point(194, 503);
+            this.buttonMirrorImage.Name = "buttonMirrorImage";
+            this.buttonMirrorImage.Size = new System.Drawing.Size(100, 30);
+            this.buttonMirrorImage.TabIndex = 0;
+            this.buttonMirrorImage.Text = "Відзеркалити";
+            this.buttonMirrorImage.UseVisualStyleBackColor = true;
+            this.buttonMirrorImage.Click += new System.EventHandler(this.buttonMirrorImage_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 386);
-            this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.ClientSize = new System.Drawing.Size(1130, 608);
+            this.Controls.Add(this.buttonMirrorImage);
+            this.Controls.Add(this.buttonSaveImage);
+            this.Controls.Add(this.buttonLoadImage);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.buttonDraw);
+            this.Controls.Add(this.comboBoxShape);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.buttonInfo);
+            this.Controls.Add(this.textBoxInfo);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Task 1-3";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
-
         }
-
-        #endregion
-
-        private System.Windows.Forms.Label label1;
     }
 }
-
--- Lab7CSharp/Form1.cs
+++ Lab7CSharp/Form1.cs
@@ -1,23 +1,151 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Lab7CSharp
+namespace WindowsFormsApp
 {
     public partial class Form1 : Form
     {
+        private Random rand = new Random();
+        private Graphics g;
+        private Image originalImage;
+
         public Form1()
         {
             InitializeComponent();
+            g = pictureBox.CreateGraphics();
+            dataGridView.Columns.Add("Type", "Тип");
+            dataGridView.Columns.Add("Size", "Розмір");
+            dataGridView.Columns.Add("Color", "Колір");
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void buttonInfo_Click(object sender, EventArgs e)
+        {
+            textBoxInfo.Text = $"X: {Location.X}, Y: {Location.Y}";
+        }
+
+        private void buttonExit_MouseHover(object sender, EventArgs e)
+        {
+            int newX = rand.Next(0, this.ClientSize.Width - buttonExit.Width);
+            int newY = rand.Next(0, this.ClientSize.Height - buttonExit.Height);
+
+            buttonExit.Location = new Point(newX, newY);
+            buttonExit.BringToFront();
+        }
+
+        private void buttonDraw_Click(object sender, EventArgs e)
+        {
+            string shape = comboBoxShape.SelectedItem.ToString();
+            switch (shape)
+            {
+                case "Коло":
+                    Circle circle = new Circle(rand.Next(50, 600), rand.Next(5, 400), rand.Next(20, 150), Color.Red);
+                    circle.Draw(g);
+                    dataGridView.Rows.Add("Коло", circle.Width, circle.Color);
+                    break;
+                case "Квадрат":
+                    Square square = new Square(rand.Next(50, 600), rand.Next(5, 400), rand.Next(20, 150), Color.Blue);
+                    square.Draw(g);
+                    dataGridView.Rows.Add("Квадрат", square.Width, square.Color);
+                    break;
+                case "Прямокутник":
+                    RectangleWithText rect = new RectangleWithText(rand.Next(50, 600), rand.Next(5, 400), rand.Next(50, 200), rand.Next(50, 200), "Текст", Color.Green);
+                    rect.Draw(g);
+                    dataGridView.Rows.Add("Прямокутник", $"{rect.Width}x{rect.Height}", rect.Color);
+                    break;
+            }
+        }
+
+        private void buttonLoadImage_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                originalImage = Image.FromFile(openFileDialog.FileName);
+                pictureBox.Image = originalImage;
+            }
+        }
+
+        private void buttonSaveImage_Click(object sender, EventArgs e)
+        {
+            Image imageToSave = pictureBox.Image;
+            if (imageToSave != null && saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                imageToSave.Save(saveFileDialog.FileName);
+            }
+        }
+
+        private void buttonMirrorImage_Click(object sender, EventArgs e)
+        {
+            if (originalImage != null)
+            {
+                Bitmap mirroredImage = new Bitmap(originalImage);
+                mirroredImage.RotateFlip(RotateFlipType.RotateNoneFlipX);
+                pictureBox.Image = mirroredImage;
+            }
+        }
+
+        abstract class Shape
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+            public int Width { get; set; }
+            public int Height { get; set; }
+            public Color Color { get; set; }
+
+            public Shape(int x, int y, int width, int height, Color color)
+            {
+                X = x;
+                Y = y;
+                Width = width;
+                Height = height;
+                Color = color;
+            }
+
+            public abstract void Draw(Graphics g);
+        }
+
+        class Circle : Shape
+        {
+            public Circle(int x, int y, int size, Color color) : base(x, y, size, size, color) { }
+
+            public override void Draw(Graphics g)
+            {
+                g.FillEllipse(new SolidBrush(Color), X, Y, Width, Height);
+            }
+        }
+
+        class Square : Shape
+        {
+            public Square(int x, int y, int size, Color color) : base(x, y, size, size, color) { }
+
+            public override void Draw(Graphics g)
+            {
+                g.FillRectangle(new SolidBrush(Color), X, Y, Width, Height);
+            }
+        }
+
+        class RectangleWithText : Shape
+        {
+            public string Text { get; set; }
+
+            public RectangleWithText(int x, int y, int width, int height, string text, Color color) : base(x, y, width, height, color)
+            {
+                Text = text;
+            }
+
+            public override void Draw(Graphics g)
+            {
+                g.FillRectangle(new SolidBrush(Color), X, Y, Width, Height);
+                g.DrawString(Text, new Font("Arial", 10), Brushes.White, X + (Width - Text.Length * 5) / 2, Y + Height / 2 - 5);
+            }
+        }
+
+        private void dataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void textBoxInfo_TextChanged(object sender, EventArgs e)
         {
 
         }
